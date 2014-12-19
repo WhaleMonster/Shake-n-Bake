2f954c185149cc738fe9e74baa08436b586e1b77
Kevin Xu <xuboxes@gmail.com>
2014-12-18 18:56:38 -0800

    blah

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,82 +1,88 @@
-get '/' do
-  if session[:id]
-      @user = User.find_by(session[:id])
-    end
-  @users = User.all
+get "/" do
   erb :index
 end
 
-#----------- SESSIONS -----------
-
+# ------------- SESSIONS --------------
+# shows sign in page if user isn't logged in
 get '/sessions/new' do
-  # render sign-in page
-  erb :sign_in
+  erb :index
 end
 
+# creates a new user in the database and sets a session
 post '/sessions' do
-  @user = User.find_by(email: params[:email])
-  if @user && @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    redirect '/'
+  if user = User.authenticate_with(params[:user])
+    session[:user_id] = user.id
+    redirect "/sessions/#{user.id}/profile"
   else
-    redirect '/sessions/new'
-  end
-  # sign-in
-end
-
-delete '/sessions/:id' do
-  # sign-out -- invoked
-  session[:user_id] = nil
-  redirect '/'
-end
-
-#----------- USERS -----------
-
-get '/users/new' do
-  # render sign-up page
-  erb :sign_up
-end
-
-post '/users' do
-  # sign-up a new user
-  @user = User.new(params[:user])
-
-  if @user.save
-    session[:user_id] = @user.id
-    redirect '/'
-  else
-    redirect '/'
+    erb :index
   end
 end
 
 # ------------- USERS ---------------
-delete '/:user_id/profile' do
+# directs the user to the the index page to sign up
+get '/users/new' do
+  erb :index
+end
+
+# create new user and redirect to user homepage
+post '/users' do
+  user = User.create(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    redirect "/sessions/#{user.id}/profile" # (need to implement user url page)
+  else
+    erb :index
+  end
+end
+
+# ----------- USER PROFILE PAGE ----------------
+
+get '/sessions/:user_id/profile' do
+  @user = User.find(params[:user_id])
+  @surveys = @user.surveys
+  erb :profile
+end
+
+# ------------ CREATE NEW SURVEY PAGE -----------
+
+get '/sessions/:user_id/surveys/new' do
+  @user = User.find(params[:user_id])
+  erb :new
+end
+
+post '/sessions/:user_id/surveys' do
+  new_survey = Survey.create(title: params[:title])
+  current_user.surveys << new_survey
+  params.keys.each do |key|
+    if key.include?('question')
+     Question.create(question: params[key], survey_id: new_survey.id)
+    end
+  end
+
+end
+# ------------- USERS ---------------
+
+delete '/sessions/:user_id/profile' do
   session[:user_id] = nil
   redirect '/'
 end
 
-get '/:user_id/profile' do
+get '/sessions/:user_id/surveys/:survey_id' do
 end
 
-get '/:user_id/surveys/:survey_id' do
+get '/sessions/:user_id/surveys/:survey_id/edit' do
 end
 
-post '/:user_id/surveys/:survey_id/new' do
+put '/sessions/:user_id/surveys/:survey_id/edit' do
 end
 
-get '/:user_id/surveys/:survey_id/edit' do
+delete '/sessions/:user_id/surveys/:survey_id/edit' do
 end
 
-put '/:user_id/surveys/:survey_id/edit' do
+get '/sessions/:user_id/surveys/:survey_id/results' do
 end
 
-delete '/:user_id/surveys/:survey_id/edit' do
-end
-
-get '/:user_id/surveys/:survey_id/results' do
-end
-
-get '/surveys/:survey_id' do
+get '/sessions/surveys/:survey_id' do
 end
 
 get '/merci' do
