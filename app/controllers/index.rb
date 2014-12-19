1b999512a01437ad5008392b43d35a991aa0623e
Anna Wu <shinshinwu@gmail.com>
2014-12-18 19:46:14 -0800

    finished all user survey routes except for question delete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -49,13 +49,20 @@ get '/sessions/:user_id/profile' do
   erb :profile
 end
 
-# ------------ CREATE NEW SURVEY PAGE -----------
-
+# create new survey
 get '/sessions/:user_id/surveys/new' do
   @user = User.find(params[:user_id])
   erb :new
 end
 
+# show specific survey
+get '/sessions/:user_id/surveys/:survey_id' do
+  @user = User.find(params[:user_id])
+  @survey = Survey.find(params[:survey_id])
+  erb :survey
+end
+
+# create a new survey
 post '/sessions/:user_id/surveys' do
   new_survey = Survey.create(title: params[:title])
   current_user.surveys << new_survey
@@ -64,15 +71,45 @@ post '/sessions/:user_id/surveys' do
      Question.create(question: params[key], survey_id: new_survey.id)
     end
   end
-
+  redirect "/sessions/#{params[:user_id]}/surveys/#{new_survey.id}"
 end
 
-post '/question' do
-  # new_question = Question.create(question: params[:question])
-  # if request.xhr?
-  #   erb :_question, layout:false, locals: {question: new_question}
-  # else
-  #   redirect "/sessions/#{current_user.id}/surveys/new"
-  # end
+# edit a specific survey link
+get '/sessions/:user_id/surveys/:survey_id/edit' do
+  @user = User.find(params[:user_id])
+  @survey = Survey.find(params[:survey_id])
+  erb :edit
 end
+
+# update the survey changes from edit page
+put '/sessions/:user_id/surveys/:survey_id' do
+  @edit_survey = Survey.find(params[:survey_id])
+  @edit_survey.update(title: params[:title])
+  params.keys.each do |key|
+    if key.include?('question')
+      question_id = key[8..-1].to_i
+      @edit_question = Question.find(question_id)
+      @edit_question.update(question: params[key])
+    end
+  end
+
+  redirect "/sessions/#{params[:user_id]}/surveys/#{params[:survey_id]}"
+end
+
+# delete the specific question (how to delete a question without refresh the page and redirect/AJAX)
+# delete '/sessions/:user_id/surveys/:survey_id/:question_id' do
+#   @question = Question.find(params[:question_id])
+#   @question.destroy
+#   redirect "/sessions/#{params[:user_id]}/surveys/#{params[:survey_id]}"
+# end
+
+# delete the specific survey
+delete '/sessions/:user_id/surveys/:survey_id' do
+  @survey = Survey.find(params[:survey_id])
+  @survey.destroy
+  redirect "/sessions/#{params[:user_id]}/profile"
+end
+
+# TODO
+# take survey page for non users and update responses
 
