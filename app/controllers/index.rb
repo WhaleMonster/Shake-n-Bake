0661ca06f49079ad5b623165341b76bffa8af10c
Kevin Xu <xuboxes@gmail.com>
2014-12-18 17:42:08 -0800

    sessions update

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,38 +1,63 @@
+# ------------- SESSIONS --------------
 get "/" do
   erb :index
 end
 
-# ------------- SESSIONS --------------
-# shows sign in page if user isn't logged in
-get '/sessions/new' do
-  erb :index
+post '/user' do
+  @user = User.find(email: params[:email])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/:user_id/profile'
+  else
+    redirect '/'
+  end
 end
 
-# creates a new user in the database and sets a session
-post '/sessions' do
-  if user = User.authenticate(params[:user])
-    session[:user_id] = user.id
-    redirect "/sessions/#{user.id}"
+post '/user/new' do
+  @user = User.new(params[:user])
+  p @user
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/:user_id/profile'
   else
-    erb :index
+    redirect '/'
   end
 end
 
 # ------------- USERS ---------------
-
-# directs the user to the the index page to sign up
-get '/users/new' do
-  erb :index
+delete '/:user_id/profile' do
+  session[:user_id] = nil
+  redirect '/'
 end
 
-# create new user and redirect to user homepage
-post '/users' do
-  user = User.create(params[:user])
-  if user.save
-    session[:user_id] = user.id
-    redirect "/sessions/#{user.id}" # (need to implement user url page)
-  else
-    erb :index
-  end
+get '/:user_id/profile' do
 end
+
+get '/:user_id/surveys/:survey_id' do
+end
+
+post '/:user_id/surveys/:survey_id/new' do
+end
+
+get '/:user_id/surveys/:survey_id/edit' do
+end
+
+put '/:user_id/surveys/:survey_id/edit' do
+end
+
+delete '/:user_id/surveys/:survey_id/edit' do
+end
+
+get '/:user_id/surveys/:survey_id/results' do
+end
+
+get '/surveys/:survey_id' do
+end
+
+get '/merci' do
+end
+
+
+
+
 
