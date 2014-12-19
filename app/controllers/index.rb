dc3a64466f8d533767e3bcec45fcc82a76b728e9
Kevin Xu <xuboxes@gmail.com>
2014-12-18 18:16:03 -0800

    revert

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,24 +1,49 @@
-# ------------- SESSIONS --------------
-get "/" do
+get '/' do
+  if session[:id]
+      @user = User.find_by(session[:id])
+    end
+  @users = User.all
   erb :index
 end
 
-post '/user' do
-  @user = User.find(email: params[:email])
-  if @user && @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    redirect '/:user_id/profile'
-  else
-    redirect '/'
-  end
+#----------- SESSIONS -----------
+
+get '/sessions/new' do
+  # render sign-in page
+  erb :sign_in
 end
 
-post '/user/new' do
+post '/sessions' do
+  @user = User.find_by(email: params[:email])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    redirect '/sessions/new'
+  end
+  # sign-in
+end
+
+delete '/sessions/:id' do
+  # sign-out -- invoked
+  session[:user_id] = nil
+  redirect '/'
+end
+
+#----------- USERS -----------
+
+get '/users/new' do
+  # render sign-up page
+  erb :sign_up
+end
+
+post '/users' do
+  # sign-up a new user
   @user = User.new(params[:user])
-  p @user
+
   if @user.save
     session[:user_id] = @user.id
-    redirect '/:user_id/profile'
+    redirect '/'
   else
     redirect '/'
   end
